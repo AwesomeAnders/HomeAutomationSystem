c5dceb8648cc39a38f4a8b08da900fc45f4b983e
Andi365 <anders1998@live.dk>
2021-01-08 13:29:00 +0100

    small bug

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -23,33 +23,36 @@ public class Server {
 
         System.out.println("Remote space created");
 
-        Object[] request;
         Gson gson = new Gson();
         String jsonString;
         Message msg;
-        //Object[] arguments;
-        //String callID, f;
 
         while (true) {
             String roomUrl;
             jsonString = lobbySpace.get(new FormalField(Object.class))[0].toString();
-            System.out.println(jsonString);
             msg = gson.fromJson(jsonString, Message.class);
-            System.out.println(msg.nameID);
             System.out.println("Message was: " + msg.nameID + " " + msg.func);
 
-            Object[] s = lobbySpace.queryp(new ActualField(msg.nameID), new FormalField(String.class));
-            if (s != null) {
-                System.out.println("in here");
-            } else {
-                roomUrl = "tcp://127.0.0.1:9001/" + msg.nameID + "?keep";
-                System.out.println("Creating new room in another thread");
-                lobbySpace.put(msg.nameID);
-                rooms.put(msg.nameID);
-                new Thread(new RoomHandler(msg.nameID, lobby)).start();
+            switch (msg.func) {
+                case "add":
+                    Object[] s = lobbySpace.queryp(new ActualField(msg.nameID), new FormalField(String.class));
+                    if (s != null) {
+                        System.out.println("in here");
+                    } else {
+                        roomUrl = "tcp://127.0.0.1:9001/" + msg.nameID + "?keep";
+                        System.out.println("Creating new room in another thread");
+                        rooms.put(msg.nameID);
+                        new Thread(new RoomHandler(msg.nameID, lobby)).start();
 
-                System.out.println("Created new room with the name: " + msg.nameID + " Room size is now: " + rooms.size());
+                        System.out.println("Created new room with the name: " + msg.nameID + " Room size is now: " + rooms.size());
+                    }
+                    break;
+                case "del":
+
             }
+
+
+
         }
     }
 }
